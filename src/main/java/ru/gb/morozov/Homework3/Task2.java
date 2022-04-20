d7736743202e0899e948a230b9b0186da8566947
Артем <Homak03@mail.ru>
2022-04-20 20:20:48 +0300

    The second tack is complete

---
+++ src/main/java/ru/gb/morozov/Homework3/Task2.java
@@ -0,0 +1,12 @@
+package ru.gb.morozov.Homework3;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int[] a = new int[100];
+        for(int i = 0; i < a.length; i++){
+            a[i] = i;
+            System.out.println(a[i]);
+        }
+    }
+}
